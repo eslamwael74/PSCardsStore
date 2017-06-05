62192a67492350331f004deab64d4ed4b00fa1d9
eslamwael74 <eslam.wael.8.ew@gmail.com>
2017-06-06 01:12:59 +0200

    commit

--- app/src/main/java/com/example/eslamwael74/fireappbase/RegisterNewUser.java
+++ app/src/main/java/com/example/eslamwael74/fireappbase/RegisterNewUser.java
@@ -5,30 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.List;
 
 
 public class RegisterNewUser extends AppCompatActivity {
